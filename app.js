ffc4d9098c15e0e06c8fb714830c92ca7e024abc
cavemanken <cavemanken@yahoo.com>
2026-01-07 13:10:12 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+localStorage.removeItem("readingLocation");
+if (localStorage.getItem("bible.readingLocation") == null) {
+  localStorage.setItem("bible.readingLocation", -1);
+}
+var localStorageReadingLocation = parseInt(
+  localStorage.getItem("bible.readingLocation")
+);
+
+const app = Vue.createApp({
+  data() {
+    return {
+      readingPlan: readings,
+      readingLocation: localStorageReadingLocation,
+      readingListSize: 20,
+    };
+  },
+  computed: {
+    readingList() {
+      console.log("building...");
+      const list = [];
+      this.readingPlan.forEach((reading, index) => {
+        if (
+          index >= this.readingLocation &&
+          index < this.readingLocation + this.readingListSize
+        ) {
+          list.push(reading);
+        }
+      });
+      return list;
+    },
+  },
+  methods: {
+    buildReadingList() {
+      console.log("updating reading location", this.readingLocation);
+      this.readingLocation++;
+      this.updateReadingLocation;
+    },
+    checkboxVisibility(index) {
+      if (index === 0) {
+        return "visibility: visible";
+      } else {
+        return "visibility: hidden";
+      }
+    },
+    updateReadingLocation() {
+      localStorage.setItem("bible.readingLocation", this.readingLocation);
+      console.log("lc", localStorage.getItem("bible.readingLocation"));
+    },
+  },
+});
+
+app.mount("#plan");
