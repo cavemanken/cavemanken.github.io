8b458fe8139c61a10c463c7192c8dc01a3bb2af6
cavemanken <cavemanken@yahoo.com>
2026-01-09 14:35:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-localStorage.removeItem("readingLocation");
-if (localStorage.getItem("bible.readingLocation") == null) {
-  localStorage.setItem("bible.readingLocation", -1);
-}
-var localStorageReadingLocation = parseInt(
-  localStorage.getItem("bible.readingLocation")
-);
-
 const app = Vue.createApp({
   data() {
     return {
@@ -16,12 +8,12 @@ const app = Vue.createApp({
   },
   computed: {
     readingList() {
-      console.log("building...");
       const list = [];
       this.readingPlan.forEach((reading, index) => {
         if (
           index >= this.readingLocation &&
-          index < this.readingLocation + this.readingListSize
+          index <
+            parseInt(this.readingLocation) + parseInt(this.readingListSize)
         ) {
           list.push(reading);
         }
@@ -30,21 +22,25 @@ const app = Vue.createApp({
     },
   },
   methods: {
-    buildReadingList() {
-      console.log("updating reading location", this.readingLocation);
-      this.readingLocation++;
-      this.updateReadingLocation;
-    },
-    checkboxVisibility(index) {
-      if (index === 0) {
-        return "visibility: visible";
+    buildReadingList(forward) {
+      if (forward) {
+        this.readingLocation++;
       } else {
-        return "visibility: hidden";
+        if (this.readingLocation > 0) {
+          this.readingLocation--;
+        }
+      }
+      this.updateLocalStorage();
+    },
+    buttonDisabled(index) {
+      if (index === 0) {
+        return false;
+      } else {
+        return true;
       }
     },
-    updateReadingLocation() {
+    updateLocalStorage() {
       localStorage.setItem("bible.readingLocation", this.readingLocation);
-      console.log("lc", localStorage.getItem("bible.readingLocation"));
     },
   },
 });
