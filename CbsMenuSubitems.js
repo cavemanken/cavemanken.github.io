0a1dad5fe2f639b21b2f1cb8e902fa597e87bd37
cavemanken <cavemanken@yahoo.com>
2026-01-21 13:05:40 -0500

    Add files via upload

---
+++ CbsMenuSubitems.js
@@ -0,0 +1,54 @@
+const cbsMenuSubitems = [
+  {
+    link: 'Shepherding Through Grief.pdf',
+    linkDesc: 'Shepherding Through Grief.pdf',
+    target: '_blank',
+  },
+  {
+    link: '2025-08-20 What Does Shepherding Look Like.pdf',
+    linkDesc: 'My Notes for Shepherding.pdf',
+    target: '_blank',
+  },
+  {
+    link: 'Psalms Leadership Development Ideas.pdf',
+    linkDesc: 'Psalms Leadership Development Ideas.pdf',
+    target: '_blank',
+  },
+  {
+    link: 'Leadership Development Resources.pdf',
+    linkDesc: 'Leadership Development Resources.pdf',
+    target: '_blank',
+  },
+  {
+    link: 'What is Leadership Development.pdf',
+    linkDesc: 'What is Leadership Development.pdf',
+    target: '_blank',
+  },
+  {
+    link: 'Gospel Conversations.pdf',
+    linkDesc: 'Gospel Conversations.pdf',
+    target: '_blank',
+  },
+  {
+    link: 'Sharing the Gospel.pdf',
+    linkDesc: 'Sharing the Gospel.pdf',
+    target: '_blank',
+  },
+  {
+    link: 'Leadership Development Helps.pdf',
+    linkDesc: 'Leadership Development Helps.pdf',
+    target: '_blank',
+  },
+  {
+    link: 'AsTheDeerLyrics.pdf',
+    linkDesc: 'AsTheDeerLyrics.pdf',
+    target: '_blank',
+  },
+  // add new links above here
+  {
+    link: 'https://drive.google.com/drive/folders/1oPPRU47CBxZvXY-_MjEFvzCTDmRE2vHY?usp=sharing',
+    linkDesc: 'AV Shared Google Link',
+    target: '_blank',
+  },
+];
+export default cbsMenuSubitems;
