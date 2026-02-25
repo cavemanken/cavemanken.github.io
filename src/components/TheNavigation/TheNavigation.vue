708848df5192b47f9c5297ce8b479385e1816135
Ken <cavemanken@yahoo.com>
2026-02-25 18:04:10 -0500

    Update TheNavigation.vue

--- src/components/TheNavigation/TheNavigation.vue
+++ src/components/TheNavigation/TheNavigation.vue
@@ -3,7 +3,7 @@
     <nav>
       <ul>
         <li><router-link to="/print">Print</router-link></li>
-        <li><router-link to="/rv">RV Stuff</router-link></li>
+        <li><router-link to="/rv">RV</router-link></li>
         <li><router-link to="/bucket-list">Bucket List</router-link></li>
         <!-- <li>
           <router-link to="/prog-rummy-rules">Prog Rummy Rules</router-link>
