e84a168f835e455e502a5081d3c1cc824ca83ed4
Ken <cavemanken@yahoo.com>
2026-02-25 10:24:44 -0500

    initial commit

---
+++ bibleplanapp.js
@@ -0,0 +1,56 @@
+localStorage.removeItem("readingLocation");
+if (localStorage.getItem("bible.readingLocation") == null) {
+  localStorage.setItem("bible.readingLocation", 0);
+}
+var localStorageReadingLocation = parseInt(
+  localStorage.getItem("bible.readingLocation")
+);
+
+const app = Vue.createApp({
+  data() {
+    return {
+      readingPlan: readings,
+      readingLocation: localStorageReadingLocation,
+      readingListSize: 20,
+    };
+  },
+  computed: {
+    readingList() {
+      const list = [];
+      this.readingPlan.forEach((reading, index) => {
+        if (
+          index >= this.readingLocation &&
+          index <
+            parseInt(this.readingLocation) + parseInt(this.readingListSize)
+        ) {
+          list.push(reading);
+        }
+      });
+      return list;
+    },
+  },
+  methods: {
+    buildReadingList(forward) {
+      if (forward) {
+        this.readingLocation++;
+      } else {
+        if (this.readingLocation > 0) {
+          this.readingLocation--;
+        }
+      }
+      this.updateLocalStorage();
+    },
+    buttonDisabled(index) {
+      if (index === 0) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    updateLocalStorage() {
+      localStorage.setItem("bible.readingLocation", this.readingLocation);
+    },
+  },
+});
+
+app.mount("#plan");
