776ad4d773167923d29085771694dde3928ce347
Ken <cavemanken@yahoo.com>
2026-02-28 19:56:12 -0500

    'build'

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <!-- To make changes and push to my web site:
   1) Make changes to the code and test ("npm run serve")
-  2) Use github desktop to commit AND push changes
+  2) Use github desktop to commit AND push your code changes
+  3) To update the website with the new changes:
     a) run "npm run build" to create new dist directory
     b) Use github desktop to commit and push changes again
     c) Run "npm run deploy" to push dist changes to the gh-pages branch
   ---- OR ----
-    a) run "npm run build-and-deploy" to do all of the above in one step  
-  3) gh-pages branch will automatically deploy to the website
+    a) run "npm run build-and-deploy" to do steps a-c above in one step  
+
+  ---- OR ----
+
+  To push all code changes and build changes at the same time:
+  4) run "npm run build-and-deploy" to do all of the above in one step
+
+  -------------------
+  5) gh-pages branch will automatically deploy to the website
    -->
 
 <template>
