1ab53e58519fcd0822ea303638e3079f746986d9
Ken <cavemanken@yahoo.com>
2026-04-23 12:17:03 -0400

    Update HomePage.vue

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -9,7 +9,7 @@
     <br />
     <a href="game-tracker.html">Game Tracker</a>
     <br />
-    <a href="pre-haul-checklist.html">Pre-Haul Checklist</a>
+    <a href="pre-haul-checklist.html">Pre-Haul Checklist </a>
   </base-card>
 </template>
 
